b359de7ab4b8b162ef1f9a925c56e2671fdc2dd0
Star <snabajja@gmail.com>
2025-01-25 14:18:06 -0500

    create Candidate.interface

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,15 @@
 const Nav = () => {
-  // TODO: Add necessary code to display the navigation bar and link between the pages
+  // TODO: Add necessary code to display the navigation bar and link between the pages. //home should link to candidate search onClick
   return (
-    <div>Nav</div>
-  )
+  <div>
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/SavedCandidates">Potential Candidates</a></li>
+      </ul>
+    </nav>
+  </div>
+  );
 };
 
 export default Nav;
--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,17 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+
+export interface Candidate {
+  avatar_url: string;
+  name: string;
+  company: string;
+  location: string;
+  email: string;
+  bio: string;
+  rejected: boolean;
+}
+export interface CandidateSearchProps {
+  candidates: Candidate[];
+}
+export interface CandidateSearchState {
+  candidates: Candidate[];
+}
