0fb0980fa23a816babe9720f88c8d752f0a0e54b
Star <snabajja@gmail.com>
2025-02-15 18:34:10 -0500

    chore: add lighter color to navigation links

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,14 +1,14 @@
 const Nav = () => {
-  // TODO: Add necessary code to display the navigation bar and link between the pages.
+  // display the navigation bar and link between the pages.
   return (
-  <div>
-    <nav>
-      <ul>
-       <a href="/">Home</a> 
-       <a href="/SavedCandidates">Potential Candidates</a>
-      </ul>
-    </nav>
-  </div>
+    <div>
+      <nav>
+        <ul>
+          <a href="/">Home</a>
+          <a href="/SavedCandidates">Potential Candidates</a>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
